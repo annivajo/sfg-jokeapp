e574bade4a4bba51c991217d17051bc686e12aaa
Anna Ivanova <annivajo@gmail.com>
2018-08-19 14:29:32 +0300

    Initial commit

---
+++ src/main/java/guru/springframework/sfgjpkeapp/SfgJpkeappApplication.java
@@ -0,0 +1,12 @@
+package guru.springframework.sfgjpkeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgJpkeappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfgJpkeappApplication.class, args);
+	}
+}
