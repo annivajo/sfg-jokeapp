dfcf0f82d098af56f644727f977ba4e643ba4376
Anna Ivanova <annivajo@gmail.com>
2018-08-20 17:16:22 +0300

    Add "joke" controller

---
+++ src/main/java/guru/springframework/sfgjpkeapp/controllers/JokeController.java
@@ -0,0 +1,23 @@
+package guru.springframework.sfgjpkeapp.controllers;
+
+import guru.springframework.sfgjpkeapp.services.JokeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JokeController {
+
+    private JokeService jokeService;
+
+    //Autowired
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"/",""})
+    public String showJoke(Model model){
+        model.addAttribute("joke", jokeService.getJoke());
+        return "chucknorris";
+    }
+}
---
+++ src/main/java/guru/springframework/sfgjpkeapp/services/JokeService.java
@@ -0,0 +1,6 @@
+package guru.springframework.sfgjpkeapp.services;
+
+public interface JokeService {
+
+    String getJoke();
+}
